20fb2bce2611c93fd6e7e8ca29e3327d2b86981e
eepi <eepi@bsidesoft.com>
2024-05-06 19:46:37 +0900

    충돌 해제 및 PIPE 일부 적용

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,22 @@
+import { Transform } from "class-transformer";
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @Transform(params => {
+        console.log(params);
+        return params.value;
+    })
+    @IsString()
+    @MinLength(2)
+    @MaxLength(30)
     readonly name: string;
+
+    @IsString()
+    @IsEmail()
+    @MaxLength(60)
     readonly email: string;
+
+    @IsString()
+    @Matches(/^[A-Za-z\d!@#$%^&*()]{8, 30}$/)
     readonly password: string;
 }
